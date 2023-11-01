f32350d4ee819f24f4d5c99c200db4aef1ad2edb
Amir Sobhani <amirsobhani@yahoo.com>
2023-11-01 19:23:52 +0330

    LIFE IN WEEKS

---
+++ 100 Days of Code The Complete Python Pro Bootcamp for 2023/Day-2/LIFE-IN-WEEKS.py
@@ -0,0 +1,7 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+age = int(age)
+left_year = 90 - age
+num_of_week = left_year * 52
+print(f"You have {num_of_week} weeks left.")
